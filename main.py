96e33bfa698b88cdc685e9b75eb84cc8f877ab6c
giacchitaccomo <giacchi.taccomo@gmail.com>
2026-03-09 21:28:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pathlib
 from datetime import datetime, timedelta
 import json #debugging iniziale
 from flask import Flask, render_template
+import os
 
 
 
@@ -40,11 +41,7 @@ def compiti():
     return render_template("compiti.html")
 
 
-def main():
-    app.run(debug=False)
-    pass
-    
   
   
 if __name__ == "__main__":
-    main()
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
