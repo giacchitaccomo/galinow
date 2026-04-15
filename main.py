a3f0b4de6ee43ebdf63ba15152f9a424a717dace
giacchitaccomo <giacchi.taccomo@gmail.com>
2026-04-15 19:21:16 +0200

    sistemato login, cookie, error handling

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-
 from organizers import initialize
 import logging
 import pathlib
-from datetime import datetime, timedelta
 import os
-from flask import Flask, render_template, redirect, session, url_for, request
-
+from flask import Flask, render_template, redirect, session, url_for, request, make_response
+from flask_session import Session
+from functools import wraps
 
 
 
@@ -19,68 +18,110 @@ logging.basicConfig(
     
 )
 
+
   
 
 app = Flask(__name__)
 # app.secret_key = os.getenv('SECRET_KEY')
 app.secret_key = "chiave"
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_FILE_DIR"] = "./flask_session"
 
+Session(app)
+
+
+
+def login_required(f):
+    
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not request.cookies.get("logged_in"):
+            return redirect(url_for("login"))
+        else:
+            return f(*args, **kwargs)
+    return decorated
 
 
 
 @app.route("/")
-def home():
-    if session.get("logged_in"):
-        return render_template("index.html")
-    else:
-        return redirect(url_for("login"))
+@login_required
+def index():
+    return render_template("index.html")
 
 
 @app.route("/orario_settimanale")
+@login_required
 def orario_settimanale():
-    if not session.get("logged_in"):
-        return redirect(url_for("login"))
     return render_template("orario_settimanale.html", orario_html=session.get("orario_html"))
 
 @app.route("/voti")
+@login_required
 def voti():
-    if not session.get("logged_in"):
-        return redirect(url_for("login"))
     return render_template("voti.html")
 
 @app.route("/compiti")
+@login_required
 def compiti():
-    if not session.get("logged_in"):
-        return redirect(url_for("login"))
     return render_template("compiti.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     error = None
-    if request.method == "POST":
+    username = None
+    password = None
+    auto = True
+
+    
+    if request.method == "POST": #se la richiesta è post allora sto inviando le credenziali tramite form
+        session.clear() 
         username = request.form.get("username", "").strip()
         password = request.form.get("password", "")
+        auto = False
+    else:
+        # se no è un get, tento di recuperare le credenziali tramite cookie
+        username = request.cookies.get("username")
+        password = request.cookies.get("password")
+        auto = True
 
-        if not username or not password:
-            error = "Inserisci username e password"
-        else:
-            try:
-                voti_materia, voti_tempo, compiti_materia, compiti_time, materie, orario, orario_html = initialize(path, username, password)
+    # se username e pw != None
+    if username and password:
+        try:
+            # provo a usarli per il login
+            voti_materia, voti_tempo, compiti_materia, compiti_time, materie, orario, orario_html = initialize(path, username, password)
 
-                session["logged_in"] = True
-                session["voti_materia"] = voti_materia
-                session["voti_tempo"] = voti_tempo
-                session["compiti_materia"] = compiti_materia
-                session["compiti_time"] = compiti_time
-                session["materie"] = materie
-                session["orario"] = orario
-                session["orario_html"] = orario_html
+            # salvataggio dati pesanti lato server, non permanente
+            session["voti_materia"] = voti_materia
+            session["voti_tempo"] = voti_tempo
+            session["compiti_materia"] = compiti_materia
+            session["compiti_time"] = compiti_time
+            session["materie"] = materie
+            session["orario"] = orario
+            session["orario_html"] = orario_html
 
-                return redirect(url_for("home"))
-            except Exception as exc:
-                logging.exception("Login failed")
+            # CREAZIONE RISPOSTA CON REDIRECT + COOKIE
+            resp = make_response(redirect(url_for("index")))
+            resp.set_cookie("username", username, max_age=60*60*24*7) # 7 giorni
+            resp.set_cookie("password", password, max_age=60*60*24*7)
+            resp.set_cookie("logged_in", "True", max_age=60*60*24*7)
+            
+            return resp
+
+        except Exception as exc:
+            logging.exception("Login failed")
+            # Se POST => è fallito un login manuale
+            if request.method == "POST":
                 error = f"Errore di accesso: {exc}"
+            else:
+                # se GET => fallimento auto login, credenziali non valide, reset
+                resp = make_response(render_template("login.html", error=None))
+                resp.set_cookie("username", "", expires=0)
+                resp.set_cookie("password", "", expires=0)
+                return resp
+    elif not auto: # se non ho username e pw e non si tratta di un tentativo automatico
+        error = "Inserire username e password"
 
     return render_template("login.html", error=error)
         
